61496f9b8a5d447f16e653cdb0dd2d7efc696e16
McKay Waters <mckaypaswaters@gmail.com>
2019-10-22 16:50:04 -0600

    about layout done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react'
 import './about.scss'
+import aboutImg from '../../assets/about_page.JPG'
 
 export default class About extends Component {
     render(){
         return (
             <div id="about">
+                <div className="about-push"></div>
+                <div className="about-container">
+                    <div className="about-img-container">
+                        <img className='about-img' src={aboutImg} alt=""/>
+                    </div>
+                    <div className="about-text-container">
 
+                    </div>
+                </div>
             </div>
         )
     }
