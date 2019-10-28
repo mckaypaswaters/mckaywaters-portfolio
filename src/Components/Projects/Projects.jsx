181d8b11afb64800c20b7a0d3345f4fe5508f8f5
McKay Waters <mckaypaswaters@gmail.com>
2019-10-28 10:07:06 -0600

    Projects section is set up!

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,6 @@
 import React, {Component} from 'react'
 import './projects.scss'
 import Slider from 'react-slick'
-import image1 from '../../assets/project-pics/canon-1.png'
-import image2 from '../../assets/project-pics/canon-2.png'
-import image3 from '../../assets/project-pics/canon-3.png'
-import image4 from '../../assets/project-pics/canon-4.png'
 
 export default class Projects extends Component {
     render(){
@@ -18,27 +14,55 @@ export default class Projects extends Component {
             slidesToScroll: 1,
             fade:true,
             cssEase: 'linear',
-            autoplaySpeed: 5000
+            autoplaySpeed: 4000
         }
         return (
             <div id="projects">
                 <div className="projectPush"></div>
                 <div className="project-container">
-                        <div className="projectOne">
-                            <Slider {...settings}>
-                                <img className='projectsImg' src={image1} alt=""/>
-                                <img className='projectsImg' src={image2} alt=""/>
-                                <img className='projectsImg' src={image3} alt=""/>
-                                <img className='projectsImg' src={image4} alt=""/>
-                            </Slider>
+                    <div className="personal-holder">
+                        <a href="https://www.canon-creative.com">
+                            <h1>Canon Creative</h1>
+                        </a>
+                        <div className="project-photos-holder">
+                            <div className="projectOne">
+                                <Slider {...settings}>
+                                    <div className="projectsImg1"></div>
+                                    <div className="projectsImg2"></div>
+                                    <div className="projectsImg3"></div>
+                                    <div className="projectsImg4"></div>
+                                </Slider>
+                            </div>
                         </div>
-                    <div className="projectTwo">
-                        <Slider {...settings}>
-                            <img className='projectsImg' src="https://images.unsplash.com/photo-1517649281203-dad836b4abe5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
-                            <img className='projectsImg' src="https://images.unsplash.com/photo-1543869664-290274af6667?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
-                            <img className='projectsImg' src="https://images.unsplash.com/photo-1546146020-c84c6bf355bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80" alt=""/>
-                            <img className='projectsImg' src="https://images.unsplash.com/photo-1528313316397-ddee14ed98f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
-                        </Slider>
+                        <div className="list-holder">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores adipisci aliquam quasi expedita cum necessitatibus animi quod, quidem deleniti</li>
+                                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem quasi ex nostrum fuga neque, veritatis laborum cumque dolores laudantium</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="personal-holder">
+                        <a href="https://www.canon-creative.com">
+                            <h1>Waka<sup>2</sup></h1>
+                        </a>
+                        <div className="project-photos-holder">
+                            <div className="projectOne">
+                                <Slider {...settings}>
+                                    <div className="projectsImg5"></div>
+                                    <div className="projectsImg6"></div>
+                                    <div className="projectsImg7"></div>
+                                    <div className="projectsImg8"></div>
+                                </Slider>
+                            </div>
+                        </div>
+                        <div className="list-holder">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores adipisci aliquam quasi expedita cum necessitatibus animi quod, quidem deleniti</li>
+                                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem quasi ex nostrum fuga neque, veritatis laborum cumque dolores laudantium</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
