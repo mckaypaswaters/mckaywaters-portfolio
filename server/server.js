96a22071bea91f32b57e8f76431b55d56e1b124f
McKay Waters <mckaypaswaters@gmail.com>
2019-11-20 12:05:48 -0700

    added a server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const {SERVER_PORT} = process.env
+const app = express()
+
+app.use(express.json())
+
+app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} red balloons`))
