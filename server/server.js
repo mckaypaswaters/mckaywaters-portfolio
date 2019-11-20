a02cc5edd238e05de74cb545c9b3ed5979c9534d
McKay Waters <mckaypaswaters@gmail.com>
2019-11-20 12:10:37 -0700

    added build static

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const {SERVER_PORT} = process.env
 const app = express()
 
+app.use(express.static(`${__dirname}/../build`))
+
 app.use(express.json())
 
 app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} red balloons`))
