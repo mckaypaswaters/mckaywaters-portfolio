5bcc2f3a7730f0c8848cae0fa2f213d004ab3a89
McKay Waters <mckaypaswaters@gmail.com>
2019-11-27 23:17:38 -0700

    about is mobile responsive

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,8 +15,8 @@ export default class About extends Component {
                         <ul>
                             <li>Hi my name is McKay Waters and I am a Full-Stack Developer from Pleasant Grove, Utah. I've always had a passion for tech and coding has been that exact fit for me. I love it and I love to continue learning new things.</li>
                             <li>If I'm not coding you can usually find me either with friends or playing video games. Gaming has been a big passion for me my entire life. I've even dabbled in streaming on Twitch and really enjoyed it.</li>
-                            <li>Music has also been a passion for me. Though I personally am not very musically talented, I almost always have music playing. Music has connected with me on so many levels that you can find me listening to anything from John Denver to Twenty One Pilots, or Taylor Swift to A Day to Remember</li>
-                            <li>My family and faith are my foundation. I am the youngest of 6 and I love each member of my family to death. I am a member of the Church of Jesus Christ of Latter-Day Saints and served a two year service mission in Lithuania. I enjoyed every minute of it and love the people of Lithuania.</li>
+                            <li className='music-li'>Music has also been a passion for me. Though I personally am not very musically talented, I almost always have music playing. Music has connected with me on so many levels that you can find me listening to anything from John Denver to Twenty One Pilots, or Taylor Swift to A Day to Remember</li>
+                            <li className='fam-li'>My family and faith are my foundation. I am the youngest of 6 and I love each member of my family to death. I am a member of the Church of Jesus Christ of Latter-Day Saints and served a two year service mission in Lithuania. I enjoyed every minute of it and love the people of Lithuania.</li>
                         </ul>
                     </div>
                 </div>
